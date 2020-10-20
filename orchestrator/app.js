62da907abeea6d3a74109f3782be27669642e6e4
rrizkie <rizkieawanj@gmail.com>
2020-10-20 11:12:13 +0700

    delte socket

--- orchestrator/app.js
+++ orchestrator/app.js
@@ -1,7 +1,6 @@
 const { ApolloServer, gql, makeExecutableSchema } = require('apollo-server')
 const userSchema = require('./schema/userSchema')
 const friendSchema = require('./schema/friendSchema')
-const socketio = require('socket.io')
 const roomName = require('./helper/generateRoomName')
 const typeDefs = gql `
     type Query
@@ -16,24 +15,8 @@ const schema = makeExecutableSchema({
 const server = new ApolloServer({schema})
 
 
-const http = server.listen().then(({url}) =>{
+server.listen().then(({url}) =>{
     console.log(`🚀 Server Go!! ${url}`);
 })
 
-const io = socketio(http)
 
-io.on('connection',socket=>{
-    console.log(`userId connected :`,socket.id)
-
-    socket.on('create_room',name=>{
-        const room = roomName(name)
-        socket.join(room)
-        socket.emit('new_room',name)
-        io.emit('created_room',room)
-    })
-
-    socket.on('join_room',data=>{
-        socket.join(data.room)
-        io.to(data.room).emit('update_player',data.username)
-    })
-})
